7e95bb3599709cd9c706f2c0427ae930c9d94ef3
teresachn <33697410+teresachn@users.noreply.github.com>
2018-10-22 15:48:14 +0700

    Preparing for book detail

---
+++ bookdetail.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Pro Book - Search Book</title>
+		<link rel="stylesheet" type="text/css" href="./searchbook.css">
+	</head>
+	<body>
+		<div>
+			<div class="header">
+				<div class="info" id="store-name">
+					<p>Pro</p>
+					- Book
+				</div>
+				<div class="info" id="user">
+					<p><u>Hi, tayotayo</u></p>
+				</div>
+				<div class="info" id="logout">
+					<img src="./icon/io.png" width="40px" height="45px">
+				</div>
+			</div>
+			<div class="menus">
+				<!-- Warna page yang sedang dipilih = #F26600-->
+				<div class="menu" id="browse">
+					<p>Browse</p>
+				</div>
+				<div class="menu" id="history">
+					<p>History</p>
+				</div>
+				<div class="menu" id="profile">
+					<p>Profile</p>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class = "search">
+			<h1>Search Result</h1>
+				<div class = "result">
+					<?php
+						$servername = "localhost";
+						$username = "root";
+						# $password = "password";
+
+						$conn = new mysqli($servername, $username, "", "wbd01");
+
+						if ($conn->connect_error) {
+							die("Connection failed: " . $conn->connect_error);
+						}
+
+						$conn->close();
+					?>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
