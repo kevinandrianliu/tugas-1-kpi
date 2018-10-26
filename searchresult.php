e7fae44897552a51668242bafdc8cf4369a0b7d7
teresachn <33697410+teresachn@users.noreply.github.com>
2018-10-26 11:25:13 +0700

    Fixed profile link

--- searchresult.php
+++ searchresult.php
@@ -58,7 +58,7 @@
 				<div class="menu" id="history" onclick="location.href='history.php'">
 					<p>History</p>
 				</div>
-				<div class="menu" id="profile" onclick="location.href='edit_profile.php'">
+				<div class="menu" id="profile" onclick="location.href='profile.php'">
 					<p>Profile</p>
 				</div>
 			</div>
